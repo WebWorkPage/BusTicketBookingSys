d2ca5707b6e1720d4323783e440e6d261227fd98
WebWorkPage <98552912+WebWorkPage@users.noreply.github.com>
2025-12-28 00:14:33 +0530

    Update AxiosInteraction.js

--- BusTicket-booking-frontend/src/utils/AxiosInteraction.js
+++ BusTicket-booking-frontend/src/utils/AxiosInteraction.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const REST_API_BASE_URL = "http://localhost:8081";
 
+export const REACT_APP_API_URL = "https://backend-production-5a1a.up.railway.app";
+
 export const setAuthHeader = () => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -9,4 +11,5 @@ export const setAuthHeader = () => {
     } else {
         delete axios.defaults.headers.common["Authorization"];
     }
+
 };
